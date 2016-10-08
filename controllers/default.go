8096b247a0610706487defdd9d7adf7447d093d9
lesxc <lesxc@163.com>
2016-10-08 13:57:13 +0800

    新增vue和html配置

--- controllers/default.go
+++ controllers/default.go
@@ -2,14 +2,22 @@ package controllers
 
 import (
 	"github.com/astaxie/beego"
+	"fmt"
 )
 
 type MainController struct {
 	beego.Controller
 }
 
+// Get 返回多个值
 func (c *MainController) Get() {
-	c.Data["Website"] = "beego.me"
-	c.Data["Email"] = "astaxie@gmail.com"
-	c.TplName = "index.tpl"
+	//c.Data["Website"] = "beego.me"
+	//c.Data["Email"] = "astaxie@gmail.com"
+
+	var user string
+	c.Data["user"]= user
+	fmt.Println(user)
+	c.TplName = "index.html"
+
+
 }
